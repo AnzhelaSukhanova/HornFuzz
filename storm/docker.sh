bec4f9ae42199e62313c4328f88c37ac1821e1c3
AnzhelaSukhanova <bidelya@gmail.com>
2022-03-15 19:49:07 +0300

    correct storm dockerfile

--- storm/docker.sh
+++ storm/docker.sh
@@ -1,7 +1,9 @@
 LOG_FILE="$PWD/storm-log"
+WORK_DIR="$PWD/workdir"
 
 touch "$LOG_FILE"
 docker run -it \
   -v "$LOG_FILE":/logfile \
+  -v "$WORK_DIR":/storm/temp \
   --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
   storm
