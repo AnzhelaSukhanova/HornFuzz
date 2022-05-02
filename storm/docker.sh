8604a2cfc22cf4fb0e57b9e79f699914510594f3
AnzhelaSukhanova <bidelya@gmail.com>
2022-05-02 20:08:48 +0300

    remove storm folder and license

--- storm/docker.sh
+++
@@ -1,9 +0,0 @@
-LOG_FILE="$PWD/storm-log"
-WORK_DIR="$PWD/workdir"
-
-touch "$LOG_FILE"
-docker run -it \
-  -v "$LOG_FILE":/logfile \
-  -v "$WORK_DIR":/storm/temp \
-  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-  storm
