332ec3d3a039de130d6d7e793cb582f2f3d4d6ec
Anzhela Sukhanova <bidelya@gmail.com>
2023-05-22 14:49:48 +0300

    correct fuzzer run script

--- docker.sh
+++ docker.sh
@@ -1,9 +1,16 @@
 CONTAINER_NUM=1
 
+timestamp() {
+  return date + %F_%T
+}
+
 run_container() {
 	docker container rm "hornfuzz$1"
+	log_dir="logs$1"
+	time=timestamp
+	touch "$log_dir/$time"
 	docker run -it \
-		-v "$PWD/log$1":/logfile \
+		-v "$PWD/$log_dir/$time":/logfile \
 		-v "$PWD/hornfuzz-workdir$1":/output \
 		--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 		--name "hornfuzz$1" \
@@ -20,8 +27,8 @@ fi
 
 for i in `eval echo {1..$CONTAINER_NUM}`; do
 	if [[ "$@" =~ "--clean-last" ]]; then
-		rm -rf "log$i" "hornfuzz-workdir$i"
-		touch "log$i"
+		rm -rf "logs$i" "hornfuzz-workdir$i"
+		mkdir "logs$i"
 	fi
 	run_container $i
 done
@@ -32,7 +39,6 @@ do
 	for i in `eval echo {1..$CONTAINER_NUM}`; do
 		status=`docker inspect -f "{{.State.Status}}" "hornfuzz$i"`
 		if [[ $status = "exited" ]]; then
-			echo $i
 			run_container $i
 		fi
 	done
